ff98ab3ba5f3ab1f3269d3f03af31b6b0bafa228
Celso Ribeiro <celsofilho82@gmail.com>
2021-05-23 13:36:47 -0300

    Implementando comportamentos por estado

--- bytebank2/lib/screens/contacts_list.dart
+++ bytebank2/lib/screens/contacts_list.dart
@@ -13,30 +13,37 @@ class ContactsList extends StatelessWidget {
       ),
       body: FutureBuilder<List<Contact>>(
         initialData: [],
-        future: Future.delayed(Duration(seconds: 1)).then((value) => findAll()),
+        future: findAll(),
         builder: (context, snapshot) {
-          if (snapshot.data != null) {
-            final List<Contact> contacts = snapshot.data;
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                final Contact contact = contacts[index];
-                return _ContactItem(
-                  contact: contact,
-                );
-              },
-              itemCount: contacts.length,
-            );
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text('Loading'),
+                  ],
+                ),
+              );
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(
+                    contact: contact,
+                  );
+                },
+                itemCount: contacts.length,
+              );
           }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                CircularProgressIndicator(),
-                Text('Loading'),
-              ],
-            ),
-          );
+          return Text('Unknown Error!!!');
         },
       ),
       floatingActionButton: FloatingActionButton(

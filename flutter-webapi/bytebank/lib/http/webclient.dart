607cbe5752f90512f41dfe7c490fcbf0cc161a7e
Celso Ribeiro <celsofilho82@gmail.com>
2021-05-25 11:05:11 -0300

    Configurando o logging interceptor

--- flutter-webapi/bytebank/lib/http/webclient.dart
+++ flutter-webapi/bytebank/lib/http/webclient.dart
@@ -1,7 +1,30 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
 
 Future<void> findAll() async {
+  final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
+  );
   final Uri url = Uri.tryParse('http://192.168.0.109:8080/transactions');
-  final Response response = await get(url);
-  print(response.body);
+  final Response response = await client.get(url);
+}
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({RequestData data}) async {
+    print('Request');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({ResponseData data}) async {
+    print('Response');
+    print('status code: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
 }

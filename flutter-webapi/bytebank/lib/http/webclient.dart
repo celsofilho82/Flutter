8dd44625fbf55a1ed7a1443a3bd01618bdd1d79f
Celso Ribeiro <celsofilho82@gmail.com>
2021-05-25 21:55:06 -0300

    Convertendo json para objeto

--- flutter-webapi/bytebank/lib/http/webclient.dart
+++ flutter-webapi/bytebank/lib/http/webclient.dart
@@ -1,12 +1,32 @@
+import 'dart:convert';
+
+import 'package:bytebank2/models/contact.dart';
+import 'package:bytebank2/models/transaction.dart';
+import 'package:bytebank2/screens/transactions_list.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
-Future<void> findAll() async {
+Future<List<Transaction>> findAll() async {
   final Client client = HttpClientWithInterceptor.build(
     interceptors: [LoggingInterceptor()],
   );
   final Uri url = Uri.tryParse('http://192.168.0.109:8080/transactions');
   final Response response = await client.get(url);
+  final List<dynamic> decodedJson = jsonDecode(response.body);
+  final List<Transaction> transactions = [];
+  for (Map<String, dynamic> transactionJson in decodedJson) {
+    final Map<String, dynamic> contactJson = transactionJson['contact'];
+    final Transaction transaction = Transaction(
+      transactionJson['value'],
+      Contact(
+        0,
+        contactJson['name'],
+        contactJson['accountNumber'],
+      ),
+    );
+    transactions.add(transaction);
+  }
+  return transactions;
 }
 
 class LoggingInterceptor implements InterceptorContract {
